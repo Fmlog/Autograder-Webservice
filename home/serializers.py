f68d75ccf0467c74f5fc3e7fa04baaf87bc56f42
Femi Alogba <femialogba1@gmail.com>
2022-07-11 12:07:36 +0100

    Add Django Framework and serializer for home page Rebuilding the code from existing demo version while documenting and refractoring. After the Django App & DRF was created, serializer for /home was written before this commit.

---
+++ home/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializers(serializers.ModelSerializer):
+    """ Serializes user data"""
+    class Meta:
+        """ Meta class for a typical user"""
+        models = User
+        """Typical user has following attributes:"""
+        fields = ('id', 'name', 'email', 'password', 'is_lecturer', 'is_student', 'is_admin')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_lecturer': {'write_only': True},
+            'is_student': {'write_only': True},
+            'is_admin': {'write_only': True},
+        }
+    def create (self, validated_data):
+        """Creates users"""
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
